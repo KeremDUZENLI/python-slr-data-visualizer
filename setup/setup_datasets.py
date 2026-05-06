6a26e43a8312a818c21fe91d5776222c52c26a98
KeremDUZENLI <krmdznl97@gmail.com>
2026-05-06 19:43:54 +0200

    fix: dataset.example.csv is fixed for xamples functions

--- setup/setup_datasets.py
+++ setup/setup_datasets.py
@@ -7,7 +7,7 @@ from input._2_prepare import (
 )
 
 
-DATASET = read_dataset(csv_path="data/dataset.example.csv")
+DATASET = read_dataset(csv_path="data/dataset.csv")
 DATASET_SOFTWARE_STACKED = group_dataset_by_fields(
     datasets=[
         (
